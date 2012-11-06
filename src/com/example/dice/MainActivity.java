41fd20b64bd7ac6c0223a725a04ad010b105e919
o3o950315 <admin@ST-30>
2012-11-06 10:30:24 +0900

    그냥 주사위 게임 ver 0.1

---
+++ src/com/example/dice/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	Button btn;
+	ImageView img;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btn = (Button)findViewById(R.id.button1);
+        img = (ImageView) findViewById(R.id.imageView1);
+        
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		int random = (int)Math.round(Math.random() *5)
+		img.setImageResource(R.drawable.dice3);
+		
+	}
+}
